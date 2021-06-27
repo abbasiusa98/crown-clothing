cac3175933818404088a3ba46dcdc524bf7356af
Usama Javed <abbasiusa98@gmail.com>
2021-06-28 02:51:22 +0500

    created shop page, collection-preview component and collection-item component  also completed their styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import { Route, Switch, Link } from "react-router";
 import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop";
 
 function App() {
-  return <div>
-    <HomePage />
-  </div>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 export default App;
